e922215464c9f414defe21908c88eab3fb7ce12e
Ismael Zavala Lopez <izavalalopez@microsoft.com>
2026-02-20 12:35:56 -0600

    Add project files.

---
+++ PersonalWebTest/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalWebTest.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
